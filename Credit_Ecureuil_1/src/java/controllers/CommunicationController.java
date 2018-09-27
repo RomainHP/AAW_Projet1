adb3feed41d9de26dc056ca2db2ecc33c3ece30a
Romain <rcharpentier10@gmail.com>
2018-09-27 15:28:31 +0200

    Corrections Spring

---
+++ Credit_Ecureuil_1/src/java/controllers/CommunicationController.java
@@ -0,0 +1,32 @@
+package controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author rcharpen
+ */
+@Controller
+public class CommunicationController {
+    
+    public CommunicationController() {
+    }
+    
+    @RequestMapping(value="index", method = RequestMethod.GET)
+    protected String init(HttpServletRequest request,HttpServletResponse response) throws Exception {
+       return "index";
+    }
+    
+    @RequestMapping(value="achat_bourse", method = RequestMethod.POST)
+    public ModelAndView handleRequestInternal(
+            HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    
+}
--- Credit_Ecureuil_1/src/java/controllers/CompteController.java
+++ Credit_Ecureuil_1/src/java/controllers/CompteController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controllers;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,16 +6,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
 
 /**
  *
  * @author rcharpen
  */
 @Controller
-public class AchatBourse extends AbstractController {
+public class CompteController {
     
-    public AchatBourse() {
+    public CompteController() {
+    }
+    
+    @RequestMapping(value="index", method = RequestMethod.GET)
+    protected String init(HttpServletRequest request,HttpServletResponse response) throws Exception {
+       return "index";
     }
     
     @RequestMapping(value="achat_bourse", method = RequestMethod.POST)
