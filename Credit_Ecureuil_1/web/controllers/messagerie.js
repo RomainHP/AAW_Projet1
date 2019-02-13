e5fcdf6febae999ed402b17f32f6606918f2e491
Romain Charpentier <romain.charpentier10@gmail.com>
2019-02-13 11:55:14 +0100

    un peu de travail pendant l'anglais

--- Credit_Ecureuil_1/web/controllers/messagerie.js
+++ Credit_Ecureuil_1/web/controllers/messagerie.js
@@ -17,7 +17,7 @@
         vm.envoyerMessage = envoyerMessage;
         vm.removeMessage = removeMessage;
         
-        initController($rootScope);
+        initController($rootScope, UtilisateurService, MessageService);
 
         function initController($rootScope, UtilisateurService, MessageService) {
            vm.user = $rootScope.globals.currentUser;
@@ -33,7 +33,7 @@
             if(vm.allUsers !== null){
                 var index = vm.allUsers.mail.length;
                 document.getElementById("destinataire").innerHTML =
-                    '<option value="default" selected>Selectionner un utilisateur</option>';
+                    '<option value="default">Selectionner un utilisateur</option>';
                 for(var i = 0; i<index; i++){
                     document.getElementById('destinataire').innerHTML += 
                     '<option value="'+ vm.allUsers.mail[i] + '">' + vm.allUsers.mail[i] + '</option>';
@@ -43,43 +43,51 @@
         
         function displayMessages() {
             if (vm.messages !== null){
-                var index = vm.accounts.Compte.length;
+                var index = vm.messages.sujet.length;
                 for(var i = 0; i<index; i++){
                     document.getElementById('table').innerHTML += '<tr data-toggle="collapse" data-target=\"#msg' + cpt + '" class="accordion-toggle">'
                     + '<td>' + i+1+ '</td>'
-                    + '<td scope="row">' + vm.accounts.Compte[i].emetteur[0] + '</td>'
-                    + '<td scope="row">' + vm.accounts.Compte[i].sujet[0] + '</td>'
+                    + '<td scope="row">' + vm.messages.emetteur[i] + '</td>'
+                    + '<td scope="row">' + vm.messages.sujet[i] + '</td>'
                     + '<td scope="row">'
                     +   '<form class="form" action="vm.removeMessage()" method="post">'
                     +       '<div class="form-group mb-3">'
-                    +           '<input type=\"hidden\" class=\"form-control\" name="id" value="' + vm.accounts.Compte[i].id[0] + '">'
+                    +           '<input type=\"hidden\" class=\"form-control\" ng-model="vm.messageRemoved.id" value="' + vm.messages.id[i] + '">'
                     +           '<button type=\"submit\" class=\"btn btn-primary btn-md\">Supprimer</button>'
                     +       '</div>'
                     +   '</form>'
                     + '</td></tr>'
-                    + '<tr><td colspan="4" class="hiddenRow"><div class="accordian-body collapse" id="msg' + cpt + '">' + message.getHtmlMessage() + '</div></td></tr>'
+                    + '<tr><td colspan="4" class="hiddenRow"><div class="accordian-body collapse" id="msg' + cpt + '">' + vm.messages.message[i] + '</div></td></tr>'
                     ;
                 }
             }
         }
         
-        function envoyerMessage(destinataire, sujet, message) {
+        function envoyerMessage() {
+            alert("test");
             vm.dataLoading = true;
-            MessageService.sendMessage(destinataire, sujet, message)
-                .then(function () {
-                    FlashService.Success('Message envoyé avec succès', true);
-                    $location.path('/');
-                },
-                function (errResponse) {
-                    FlashService.Error("Erreur : " + errResponse.data["errorMessage"], true);
-                    $location.path('/');
-                }
-            );
+            var dest = document.getElementById("destinataire");
+            var destSelected = dest.options[dest.selectedIndex].value;
+            if (destSelect != 0) {
+                MessageService.sendMessage(destSelected, vm.message.sujet, vm.message.message)
+                    .then(function () {
+                        FlashService.Success('Message envoyé avec succès', true);
+                        $location.path('/');
+                    },
+                    function (errResponse) {
+                        FlashService.Error("Erreur : " + errResponse.data["errorMessage"], true);
+                        $location.path('/');
+                    }
+                );
+            } else {
+                FlashService.Error("Erreur : Veuillez saisir un destinataire", true);
+                $location.path('/envoyer_message');
+            }
         }
         
-        function removeMessage(id) {
+        function removeMessage() {
             vm.dataLoading = true;
-            MessageService.removeMessage(id)
+            MessageService.removeMessage(vm.messageRemoved.id[0])
                 .then(function () {
                     FlashService.Success('Message supprimé avec succès', true);
                     $location.path('/consulter_messagerie');
